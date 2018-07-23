c377c211932d1328c6b96555c9c4c7cc592b2319
Victoria Law <vhlaw@calpoly.edu>
2018-07-23 15:56:49 -0700

    Auto stash before merge of "master" and "origin/master"

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
+var correctAnswer = 0;
+
 document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
+    var links = document.getElementById("demo");
     for (var i = 0; i < links.length; i++) {
         (function () {
             var ln = links[i];
@@ -10,3 +12,34 @@ document.addEventListener('DOMContentLoaded', function () {
         })();
     }
 });
+
+function openNewTab() {
+var links = document.getElementById("demo");
+chrome.tabs.create({active: true, url: location});
+
+function submitFirstAnswer() {
+  var radios = document.getElementsByName("choice");
+  var i = 0, len = radios.length;
+  var checked = false;
+  var userAnswer;
+  
+  for( ; i < len; i++ ) {
+     if(radios[i].checked) {
+       checked = true;
+       userAnswer = radios[i].value;
+     }
+  } 
+  // if user click submit button without selecting any option, alert box should be say "please select choice answer".
+  if(!checked) {
+    alert("Please select choice answer.");
+    return;
+  }
+  // Correct answer
+  if(userAnswer === "Costa Rica") {
+     alert("Answer is correct!");
+  }
+  // incorrect answer
+  else {
+     alert("Answer is wrong!");
+  }
+}
